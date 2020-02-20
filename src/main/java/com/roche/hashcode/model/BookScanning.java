8dd0552167b7f22e57b781bd941001a20695206d
deolivl1 <Toreto2019>
2020-02-20 20:28:47 +0100

    Keep going

--- src/main/java/com/roche/hashcode/model/BookScanning.java
+++ src/main/java/com/roche/hashcode/model/BookScanning.java
@@ -1,14 +1,38 @@
 package com.roche.hashcode.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookScanning {
 
     private int timeAvailable;
-
-    public BookScanning(int timeAvailable) {
-        this.timeAvailable = timeAvailable;
-    }
+    private List<Library> libraries = new ArrayList<>();
 
     public int getTimeAvailable() {
         return timeAvailable;
+    }
+
+    public void setTimeAvailable(int timeAvailable) {
+        this.timeAvailable = timeAvailable;
+    }
+
+    public List<Library> getLibraries() {
+        return libraries;
+    }
+
+    public boolean addLibrary(Library library) {
+        return this.libraries.add(library);
+    }
+
+    public void setLibraries(List<Library> libraries) {
+        this.libraries = libraries;
+    }
+
+    @Override
+    public String toString() {
+        return "BookScanning{" +
+                "timeAvailable=" + timeAvailable +
+                ", libraries=" + libraries +
+                '}';
     }
 }
--- src/test/java/com/roche/hashcode/library/ScanningBooksShould.java
+++ src/test/java/com/roche/hashcode/library/ScanningBooksShould.java
@@ -1,6 +1,7 @@
 package com.roche.hashcode.library;
 
 import com.roche.hashcode.model.Book;
+import com.roche.hashcode.model.BookScanning;
 import com.roche.hashcode.model.Library;
 import org.junit.Test;
 
@@ -22,15 +23,17 @@ public class ScanningBooksShould {
         final List<String> lines = Files.readAllLines(Paths.get("src/test/resources/a_example.txt"));
         final String[] order = lines.get(0).split(" ");
         //Fixed
+        BookScanning bookScanning = new BookScanning();
+        bookScanning.setTimeAvailable(Integer.parseInt(order[2]));
+
         int books = Integer.parseInt(order[0]);
         int libraries = Integer.parseInt(order[1]);
-        int daysForScanning = Integer.parseInt(order[2]);
 
         List<Integer> scores = LibraryUtil.listOf(lines.get(1));
 
         int jumpLines = 2;
 
-        for (int index = 2; index < lines.size(); index=index*2) {
+        for (int index = jumpLines; index < lines.size(); index=index*jumpLines) {
             final String[] dataOfLibrary = lines.get(index).split(" ");
 
             HashSet<Book> booksOfFirstLibrary = new HashSet<>();
@@ -40,21 +43,9 @@ public class ScanningBooksShould {
             final Library library = new Library(booksOfFirstLibrary,
                     Integer.parseInt(dataOfLibrary[1]),
                     Integer.parseInt(dataOfLibrary[2]));
-            System.out.println(library);
-        }
 
-        /*HashSet<Book> booksOfFirstLibrary = new HashSet<>();
-        for (String score : lines.get(3).split(" ")) {
-            booksOfFirstLibrary.add(new Book(Integer.parseInt(score)));
+            bookScanning.addLibrary(library);
         }
-        final String[] dataOfLibrary = lines.get(2).split(" ");
-        final Library library = new Library(booksOfFirstLibrary,
-                Integer.parseInt(dataOfLibrary[1]),
-                Integer.parseInt(dataOfLibrary[2]));
-        System.out.println(library);*/
-        //System.out.println(books+"#"+libraries+"#"+day
-        // sForScanning);
-
+        System.out.println(bookScanning);
     }
-
 }
