81f912f4231a0201ff112ffba2b5de2be6a8db3f
Anyul Rivas <anyul.rivas_oropeza@roche.com>
2020-02-20 20:30:01 +0100

    optimisation algorithm

---
+++ src/test/java/com/roche/hashcode/library/OptimisationAlgorithmShould.java
@@ -0,0 +1,11 @@
+package com.roche.hashcode.library;
+
+import org.junit.Test;
+
+public class OptimisationAlgorithmShould {
+
+    @Test
+    public void returnOutputFileForExampleOne() {
+
+    }
+}
